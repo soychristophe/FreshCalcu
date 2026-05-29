e25c2fa4c2ea5f9d8aa5f252289c382f768f3481
Christophe Gomez de la Red <soychristophe@Christophes-MacBook-Pro.local>
2026-05-29 23:11:36 +0100

    fix: sincronizar package-lock con los tipos de cloudflare y actualizar dom

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -15,7 +15,7 @@ export function make<K extends keyof HTMLElementTagNameMap>(
   ...children: (Node | string)[]
 ): HTMLElementTagNameMap[K] {
   const node = Object.assign(document.createElement(tag), props);
-  if (children.length) node.append(...(children as [Node | string, ...(Node | string)[]]));
+  node.append(...children);
   return node;
 }
 
